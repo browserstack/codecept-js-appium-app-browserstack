48e0be2792f941a35ec7e05848430ce134b15f32
Karan Shah <64479353+karanshah-browserstack@users.noreply.github.com>
2024-10-29 00:56:54 +0530

    Initial Commit Repo

---
+++ android/src/test/sample/codecept.conf.js
@@ -0,0 +1,45 @@
+const browserstack = require("browserstack-local");
+const util = require('util');
+require('dotenv').config()
+
+const BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME
+const BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY
+
+exports.config = {
+    tests: './*_test.js',
+    output: './output',
+    timeout: 120,
+    helpers: {
+        Appium: {
+            url: 'https://bstackdemo.com',
+            app: "../WikipediaSample.apk",
+            user: BROWSERSTACK_USERNAME,
+            key: BROWSERSTACK_ACCESS_KEY,
+            host: "hub.browserstack.com",
+            port: 4444,
+            platform: "Android",
+            desiredCapabilities: {
+                'appiumV2': true, // Enable Appium 2.x
+                'browserstack.debug': true,
+                'browserstack.networkLogs': true,
+            }
+        }
+    },
+    include: {
+        I: './steps_file.js'
+    },
+    mocha: {},
+    name: 'CodeceptJS-Appium-BrowserStack',
+    plugins: {
+        pauseOnFail: {},
+        retryFailedStep: {
+            enabled: true
+        },
+        tryTo: {
+            enabled: true
+        },
+        screenshotOnFail: {
+            enabled: true
+        }
+    }
+}
